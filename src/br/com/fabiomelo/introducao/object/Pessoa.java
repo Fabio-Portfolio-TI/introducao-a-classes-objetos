034c901756af8444299a46d4ea1904991b1609b9
unknown <you@example.com>
2023-11-25 15:47:50 -0300

    Main

--- src/br/com/fabiomelo/introducao/object/Pessoa.java
+++ src/br/com/fabiomelo/introducao/object/Pessoa.java
@@ -1,5 +1,24 @@
 package br.com.fabiomelo.introducao.object;
 
-public class Pessoa {
+public class Pessoa extends Object{
+	
+	// Toda Classe herda Object
+	
+	private String nome;
+	
+	@Override
+	public String toString() {
+		return nome;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 
 }
