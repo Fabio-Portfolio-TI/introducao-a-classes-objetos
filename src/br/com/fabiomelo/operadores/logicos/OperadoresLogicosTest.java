f482f8c5f26519025b9473ada5fc4a00a8a946c1
unknown <you@example.com>
2023-11-26 21:02:49 -0300

    Operadores Or, Ternário e Negação

--- src/br/com/fabiomelo/operadores/logicos/OperadoresLogicosTest.java
+++ src/br/com/fabiomelo/operadores/logicos/OperadoresLogicosTest.java
@@ -17,9 +17,35 @@ public class OperadoresLogicosTest {
 			System.out.println("Pode fazer empréstimo.");
 		} else {
 			System.out.println("Não pode fazer empréstimo.");
+		}
+		
+		String usuario = "fabio";
+		String senha = "123456";
+		
+		if(usuario.equals("fabio1") && senha.equals("123456") || usuario.length() > 3) {
+			System.out.println("Usuário logado...");
+		}else {
+			System.out.println("Usuário ou Senha inválidos...");			
+		}
+		
+		String texto = "Jesus é a luz do mundo!!!";
+		
+		// Operador Ternário
+		boolean textoMaiorQue5 = texto.length() > 20 ? true : false;
+		System.out.println(textoMaiorQue5);
 
+		String meuTime = "Corinthians";
+		
+		System.out.println("Corinthians " + ( (meuTime.equalsIgnoreCase("Corinthians")) ? "Tem Mundial!" :  "Não tem Mundial!"));
+		
+		boolean podeAcessarSistema = true;
+		boolean somenteParaFuncionario = true;
+		
+		if (!podeAcessarSistema && somenteParaFuncionario) {
+			System.out.println("Você pode acessar os sistema!");
+		} else {
+			System.out.println("Você não pode acessar os sistema!");
 		}
 		
 	}
-
 }
